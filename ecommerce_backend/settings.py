f843a03c9b8c8b2f883e3287a04dcc17444c05ec
Swamy <88021517+Nani939@users.noreply.github.com>
2025-08-12 08:31:01 +0530

    Initial commit

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',  
+    'rest_framework',
+    'corsheaders',      # if using API + React
+    'shop',
 ]
 
 
